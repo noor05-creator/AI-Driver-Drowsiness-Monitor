d5aebde6883e65f5f0dc9a708ec3a8ad356d469e
noor05-creator <noorasif.noor05@gmail.com>
2026-05-15 03:17:26 +0500

    move commitlint.config.js to root directory

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'docs',
+      'refactor',
+      'test',
+      'chore'
+    ]],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72]
+  }
+}
